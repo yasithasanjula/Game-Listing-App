8d7c6ae5ce5cec408a694582d4f6c1e4922d26fc
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-11-18 17:26:33 +0530

    bnjh

--- Game-List/src/App.jsx
+++ Game-List/src/App.jsx
@@ -1,23 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Header from './Components/Header'
 import Home from './Pages/Home'
 import { ThemeContext } from './Context/ThemeContext'
 
 function App() {
-  const [theme,setTheme]=useState('light')
+  const [theme, setTheme] = useState('light')
+
+  useEffect(() => {
+    // Update the class for the body element based on the selected theme
+    document.body.classList.toggle('dark-theme', theme === 'dark')
+  }, [theme])
 
   return (
-    <>
-    <ThemeContext.Provider value={{theme,setTheme}}> 
-    <div className={`${theme} 
-      ${theme=='dark'?'bg-[#121212]':null}`} >
-        <Header/>
-        <Home/>
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={`${theme} ${theme === 'dark' ? 'bg-[#121212]' : ''}`}>
+        <Header />
+        <Home />
+      </div>
     </ThemeContext.Provider>
-      
-    </>
   )
 }
 
