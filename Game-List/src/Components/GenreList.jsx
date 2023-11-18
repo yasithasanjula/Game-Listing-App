bf0d3b53cb63af6832b2c903965b48f8ef321a7d
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-11-18 22:22:15 +0530

    11

--- Game-List/src/Components/GenreList.jsx
+++ Game-List/src/Components/GenreList.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import GlobalApi from '../Services/GlobalApi'
 
 function GenreList() {
+    useEffect(()=>{
+        getGenreList();
+    },[])
+    const getGenreList=()=>{
+        GlobalApi.getGenreList((resp)=>{
+            console.log(resp);
+        })
+    }
   return (
     <div>GenreList</div>
   )
--- Game-List/src/Services/GlobalApi.jsx
+++ Game-List/src/Services/GlobalApi.jsx
@@ -1,6 +1,7 @@
+import axios from 'axios';
 
 const key="dfb6e45df2314199a627dbdbd4ad700a";
-const axiosCreate=axios.axiosCreate({
+const axiosCreate=axios.create({
     baseURL:'https://rawg.io/@yasithasanjula/apikey'
 })
 
